e01dc2c2d7f2fb00209600da5586f40b9bd9fdd1
VI Nguyen <nguyenhungvicb@gmail.com>
2024-09-08 15:16:18 +0700

    😁

--- test/web.test.js
+++ test/web.test.js
@@ -1,7 +1,43 @@
-import {describe, it} from "node:test";
+import { describe, it, before } from "node:test";
+import Controller from "../src/shared/controller.js";
+import View from "../src/platforms/web/view.js";
+
+function getDocument(mock) {
+  globalThis.alert = mock.fn(() => {});
+  globalThis.document = {
+    createElement: mock.fn((name) => {
+      return {
+        classList: {
+          add: mock.fn((name) => {}),
+        },
+      };
+    }),
+    querySelector: mock.fn((id) => {
+      return {
+        appendChild: mock.fn((child) => {}),
+        reset: mock.fn(() => {}),
+        addEventListener: mock.fn((event, fn) => {
+          return fn({
+            preventDefault: () => {},
+          });
+        }),
+      };
+    }),
+  };
+
+  return globalThis.document;
+}
 
 describe("web app test suite", () => {
-  it("given valid input, should update the table", async () => {
-    console.log("working");
+  let _controller;
+
+  it("given valid input, should update the table", async (context) => {
+    const mock = context.mock;
+    const document = getDocument(mock);
+    _controller = Controller.init({
+      view: new View(),
+    });
+
+    document.querySelector.mock;
   });
-})
+});
