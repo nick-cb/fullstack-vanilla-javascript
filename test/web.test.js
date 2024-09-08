88485fed4eb9651817eb26c951c0e6c2074c8746
VI Nguyen <nguyenhungvicb@gmail.com>
2024-09-08 14:49:15 +0700

    😄

---
+++ test/web.test.js
@@ -0,0 +1,7 @@
+import {describe, it} from "node:test";
+
+describe("web app test suite", () => {
+  it("given valid input, should update the table", async () => {
+    console.log("working");
+  });
+})
