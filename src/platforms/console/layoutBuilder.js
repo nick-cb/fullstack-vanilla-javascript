bc565375828dcb593f23518458629a7d07ab91c3
VI Nguyen <nguyenhungvicb@gmail.com>
2024-09-10 22:15:31 +0700

    🥲

---
+++ src/platforms/console/layoutBuilder.js
@@ -0,0 +1,178 @@
+import blessed from "blessed";
+import contrib from "blessed-contrib";
+
+export class LayoutBuilder {
+  /** @type blessed.Widgets.Screen */
+  #screen;
+  #layout;
+  #form;
+  #input = {};
+  #buttons = {};
+  #alert;
+
+  setScreen({ title }) {
+    this.#screen = blessed.screen({
+      smartCSR: true,
+      title: title,
+    });
+
+    this.#screen.key(["escape", "q", "C-c"], () => process.exit(0));
+
+    return this;
+  }
+
+  setLayout() {
+    this.#layout = blessed.layout({
+      parent: this.#screen,
+      width: "100%",
+      height: "100%",
+    });
+
+    return this;
+  }
+
+  #createInputField({ parent, name, top, label }) {
+    const input = blessed.textbox({
+      parent,
+      name,
+      top,
+      label,
+      inputOnFocus: true,
+      left: "center",
+      width: "60%",
+      height: "20%",
+      border: { type: "line" },
+      styles: { fg: "white", bg: "blue", focus: { bg: "lightblue" } },
+    });
+
+    return input;
+  }
+
+  #createButton({ parent, name, content, bg, fg, left, bottom }) {
+    return blessed.button({
+      parent,
+      name,
+      left,
+      bottom,
+      content,
+      mouse: true,
+      keys: true,
+      shrink: true,
+      padding: { left: 1, right: 1 },
+      width: "shrink",
+      style: { bg, fg, focus: `light${bg}`, hover: `light${bg}` },
+    });
+  }
+
+  setFormComponent({ onSubmit, onClear }) {
+    const form = blessed.form({
+      parent: this.#layout,
+      keys: true,
+      vi: true,
+      width: "100%",
+      height: "40%",
+      top: 0,
+      left: "center",
+      label: "Users form",
+      border: { type: "line" },
+      style: { fg: "white", bg: "black" },
+    });
+
+    const nameInput = this.#createInputField({
+      parent: form,
+      name: "name",
+      top: 1,
+      label: "Name:",
+    });
+    nameInput.focus();
+    const ageInput = this.#createInputField({
+      parent: form,
+      name: "age",
+      top: 4,
+      label: "Age:",
+    });
+    const emailInput = this.#createInputField({
+      parent: form,
+      name: "email",
+      top: 7,
+      label: "Email:",
+    });
+    const submitBtn = this.#createButton({
+      parent: form,
+      name: "submit",
+      content: "Submit",
+      bottom: 1,
+      bg: "green",
+      fg: "black",
+      left: "46%",
+    });
+    submitBtn.on("press", () => form.submit());
+    form.on("submit", (data) => {
+      onSubmit(data);
+    });
+    const clearBtn = this.#createButton({
+      parent: form,
+      name: "clear",
+      content: "Clear",
+      bottom: 1,
+      bg: "red",
+      fg: "black",
+      left: "52%",
+    });
+    clearBtn.on("press", () => onClear());
+
+    this.#form = form;
+    this.#input.name = nameInput;
+    this.#input.age = ageInput;
+    this.#input.email = emailInput;
+
+    this.#buttons.submit = submitBtn;
+    this.#buttons.clear = clearBtn;
+
+    return this;
+  }
+
+  setAlertComponent() {
+    this.#alert = blessed.box({
+      parent: this.#form,
+      width: "40%",
+      height: "20%",
+      bottom: 0,
+      border: { type: "line" },
+      style: { bg: "red", fg: "black" },
+      content: "",
+      tags: true,
+      align: "center",
+      hidden: true,
+    });
+
+    this.#alert.setMessage = (msg) => {
+      this.#alert.setContent(`{bold}${msg}{/bold}`);
+      this.#alert.show();
+      this.#screen.render();
+
+      setTimeout(() => {
+        this.#alert.hide();
+        this.#screen.render();
+      }, 3000);
+    };
+
+    return this;
+  }
+
+  setTable({numColumns}) {
+
+  }
+
+  build() {
+    const components = {
+      screen: this.#screen,
+      layout: this.#layout,
+      form: this.#form,
+      alert: this.#alert,
+    };
+    components.screen.render();
+
+    return components;
+  }
+}
--- src/platforms/console/view.js
+++ src/platforms/console/view.js
@@ -1,1 +1,52 @@
-console.log('hello console!')
+import ViewBase from "../../shared/viewBase.js";
+import { LayoutBuilder } from "./layoutBuilder.js";
+
+export default class View extends ViewBase {
+  #layoutBuilder;
+  #components;
+  #onFormSubmit = () => {};
+  #onFormClear = () => {};
+
+  constructor(layoutBuilder = new LayoutBuilder()) {
+    super();
+
+    this.#layoutBuilder = layoutBuilder;
+  }
+
+  configureFormSubmit(fn) {
+    this.#onFormSubmit = (data) => {
+      return fn(data);
+    };
+  }
+
+  notify({ msg, isError }) {
+    this.#components.alert.setMessage(msg);
+  }
+
+  resetForm() {
+    this.#components.form.reset();
+    this.#components.screen.render();
+  }
+
+  configureFormClear() {
+    this.#onFormClear = () => {
+      this.resetForm();
+    };
+  }
+
+  #initializeComponentFacade() {
+    this.#components = this.#layoutBuilder
+      .setScreen({ title: "Fullstack vanilla js" })
+      .setLayout()
+      .setFormComponent({
+        onClear: this.#onFormClear.bind(this),
+        onSubmit: this.#onFormSubmit.bind(this),
+      })
+      .setAlertComponent()
+      .build();
+  }
+
+  render(items) {
+    this.#initializeComponentFacade();
+  }
+}
