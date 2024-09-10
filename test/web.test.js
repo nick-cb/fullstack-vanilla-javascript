5bcf1f16346486d714005adee0230b878e40f445
VI Nguyen <nguyenhungvicb@gmail.com>
2024-09-10 09:58:04 +0700

    😆

--- test/web.test.js
+++ test/web.test.js
@@ -1,7 +1,12 @@
-import { describe, it, before } from "node:test";
+import { describe, it } from "node:test";
 import Controller from "../src/shared/controller.js";
 import View from "../src/platforms/web/view.js";
+import assert from "node:assert";
 
+/**
+ * @typedef {import('node:test').TestContext} TestContext
+ * @param {TestContext['mock']} mock
+ */
 function getDocument(mock) {
   globalThis.alert = mock.fn(() => {});
   globalThis.document = {
@@ -14,6 +19,7 @@ function getDocument(mock) {
     }),
     querySelector: mock.fn((id) => {
       return {
+        value: "test",
         appendChild: mock.fn((child) => {}),
         reset: mock.fn(() => {}),
         addEventListener: mock.fn((event, fn) => {
@@ -34,10 +40,36 @@ describe("web app test suite", () => {
   it("given valid input, should update the table", async (context) => {
     const mock = context.mock;
     const document = getDocument(mock);
-    _controller = Controller.init({
-      view: new View(),
-    });
+    const view = new View();
 
-    document.querySelector.mock;
+    const addRow = context.mock.method(view, view.addRow.name);
+    _controller = Controller.init({ view });
+
+    const [name, age, email, tableBody, form, btnFormClear] =
+      document.querySelector.mock.calls;
+
+    assert.strictEqual(name.arguments[0], "#name");
+    assert.strictEqual(age.arguments[0], "#age");
+    assert.strictEqual(email.arguments[0], "#email");
+    assert.strictEqual(tableBody.arguments[0], ".flex-table");
+    assert.strictEqual(form.arguments[0], "#form");
+    assert.strictEqual(btnFormClear.arguments[0], "#btnFormClear");
+
+    const onSubmit = form.result.addEventListener.mock.calls[0].arguments[1];
+    const preventDefaultSpy = context.mock.fn(() => {});
+    assert.strictEqual(addRow.mock.callCount(), 2);
+
+    onSubmit.apply(view, [
+      {
+        preventDefault: preventDefaultSpy,
+      },
+    ]);
+    assert.strictEqual(addRow.mock.callCount(), 3);
+
+    assert.deepStrictEqual(addRow.mock.calls.at(2).arguments.at(0), {
+      name: "test",
+      age: "test",
+      email: "test",
+    });
   });
 });
